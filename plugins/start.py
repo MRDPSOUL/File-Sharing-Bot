ce6c00c16a04c8fbe5ca8e54dfdb0164fe84d5c4
MR DPSOUL <89294756+MRDPSOUL@users.noreply.github.com>
2022-09-03 11:53:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,8 +92,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close"),
-                    InlineKeyboardButton("DPSOUL", callback_data = "close")
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
                 ]
             ]
         )
